a1457ef6eb8849d1e738848b3a00e14092b72e6c
YEL MANGOK JOHN <yelmangok1992@gmail.com>
2026-06-09 16:01:52 +0200

    Main jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-function App() {
+import {Router, Route} Form "react-router-dom";
+
+//import the components and pages
+import Navbar from "./components/Navbar";
+//import the pages
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import TaskDetails from "./pages/TaskDetails";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+
+export default function App() {
  
   return (
     <>
-      
+      <Navbar />
+
+      <Routes>
+        <Route path ="/" element={<Home />}></Route>
+        <Route path ="/tasks" element={<Tasks />}></Route>
+        <Route path ="/tasks/:id" element={<TaskDetails />}></Route>
+        <Route path ="/about" element={<About />}></Route>
+        <Route path ="*" element={<NotFound />}></Route>
+      </Routes>
     </>
   )
 }
