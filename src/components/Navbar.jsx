9ab2f478e48dc2e324ff258213f86fb7a3fbc85f
YEL MANGOK JOHN <yelmangok1992@gmail.com>
2026-06-10 17:54:59 +0200

    New component craeted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,14 @@ export default function Navbar(){
     return (
         <nav>
             <Link to ="/">Home</Link>
+            <br/>
              <Link to ="/dashboard">Dashboard</Link>
+             <br/>
             <Link to ="/tasks">Tasks</Link>
+            <br/>
+            <Link to ="/tasksMenu">Tasks Menu</Link>
+            <br/>
             <Link to ="/about">About</Link>
-
         </nav>
     );
 }
-
