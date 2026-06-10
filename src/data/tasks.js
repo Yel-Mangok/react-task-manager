125453205f689b0666a1d8ca1a6663698e78ba5d
YEL MANGOK JOHN <yelmangok1992@gmail.com>
2026-06-10 17:52:39 +0200

    Dataset -tasks List

---
+++ src/data/tasks.js
@@ -0,0 +1,19 @@
+const tasks = [
+    {
+        id: 1,
+        title: "Build navbar",
+        status: "Completed"
+    },
+    {
+        id: 2,
+        title: "Create Dashboard",
+        status: "In progress"
+    },
+    {
+        id: 3,
+        title: "Implement Router",
+        status: "Pending"
+    }
+];
+
+export default tasks;
