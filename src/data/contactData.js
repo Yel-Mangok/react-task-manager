e537cef75a69c237016cf0b2a136f040b2b2da18
YEL MANGOK JOHN <yelmangok1992@gmail.com>
2026-06-12 17:51:31 +0200

    Dataset for Data created

---
+++ src/data/contactData.js
@@ -0,0 +1,8 @@
+const contactData = {
+    "CompanyName": "PrimeTek Africa",
+    "PhoneNumber": "+243 997300904",
+    "MailAddress": "info@primetek.africa",
+    "linkedIn": "https://www.linkedin.com/company/primetekafrica"
+};
+
+export default contactData;
