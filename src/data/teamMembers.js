7688df935289e239095aa71943d92ee5ad2306a5
YEL MANGOK JOHN <yelmangok1992@gmail.com>
2026-06-12 16:30:10 +0200

    Dataset team member created

---
+++ src/data/teamMembers.js
@@ -0,0 +1,18 @@
+const members = [
+    {
+        id: 1,
+        name: "Tony Stark",
+        role: "Back-end Developer"
+    },
+     {
+        id: 2,
+        name: "Bruce Wayne",
+        role: "Database Administrator"
+    },
+     {
+        id: 3,
+        name: "Clerk Kent",
+        role: "Fullstack Developer"
+    }
+];
+export default members;
