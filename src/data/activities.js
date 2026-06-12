7327dcbfe39607e3795ba21dca01ad80cee88931
YEL MANGOK JOHN <yelmangok1992@gmail.com>
2026-06-12 17:08:22 +0200

    Dataset activity added

---
+++ src/data/activities.js
@@ -0,0 +1,10 @@
+const activities = [
+    "Create Dashboard",
+    "Added Router",
+    "Create Admin Panel",
+    "Update settings page",
+    "Fix the profile page",
+    "improve the components styles"
+];
+
+export default activities; 
